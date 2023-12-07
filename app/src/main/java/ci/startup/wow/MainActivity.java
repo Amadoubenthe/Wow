3fe5776de009b815b9df6148189dbc31e745a1e8
Amadoubenthe <amadbenthebagnandiallo@gmail.com>
2023-12-07 11:10:16 +0000

    first commit

---
+++ app/src/main/java/ci/startup/wow/MainActivity.java
@@ -0,0 +1,14 @@
+package ci.startup.wow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_home);
+    }
+}
